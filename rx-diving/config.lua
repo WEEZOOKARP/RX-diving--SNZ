c227561fe4c3732adbe744aa4a1908c01399c16e
WEEZOOKARP <76672483+WEEZOOKARP@users.noreply.github.com>
2022-02-01 12:11:53 +1300

    Update config.lua

--- rx-diving/config.lua
+++ rx-diving/config.lua
@@ -1,3 +1,11 @@
 Config = {}
 
 Config.Notification = 'SNZ' -- Can be SNZ or QBCore
+
+Config.Materials = {
+  [1] = ('glass'),
+  [2] = ('rubber'),
+  [3] = ('steel'),
+  [4] = ('aluminum'),
+  [5] = ('metalscrap')
+}
